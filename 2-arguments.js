f80b038e5b257a5b7f74cbb228a9f03c16123be2
Tinsae <etoboytinsu@gmail.com>
2025-05-25 20:40:53 +0300

    final

---
+++ 2-arguments.js
@@ -0,0 +1,9 @@
+const args = process.argv.slice(2); // skip 'node' and script name
+
+if (args.length === 0) {
+  console.log("No argument");
+} else if (args.length === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
